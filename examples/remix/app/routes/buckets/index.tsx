fc9301f81082a1f2c8b7c50e0fc46d87ebceb05d
Andrew Jefferson <andy@mandadata.com>
2022-03-04 18:48:32 +0100

    add and list buckets

---
+++ examples/remix/app/routes/buckets/index.tsx
@@ -0,0 +1,54 @@
+import { ActionFunction, Form, useLoaderData } from "remix";
+import { s3Client } from "../../../backend/s3Client.server";
+import { createBucket, listBuckets } from "../../../backend/buckets.server";
+
+type BucketData = {
+    name: string;
+    createdAt: string;
+}
+
+type BucketsList = {
+    buckets: BucketData[];
+}
+
+export const action: ActionFunction = async ({ request }) => {
+    let { name } = Object.fromEntries(await request.formData());
+
+    if (typeof name !== "string") {
+        throw new Response("Invalid data submitted", { status: 400 });
+    }
+
+    await createBucket(s3Client, { Bucket: name as string });
+    console.log("created bucket", name);
+    return "success";
+};
+
+export async function loader() {
+    const buckets = await listBuckets(s3Client, {});
+    return {
+        buckets: buckets.map(bucket => ({ name: bucket.Name, createdAt: bucket.CreationDate?.toISOString() ?? "unknown" }))
+    };
+}
+
+export default function Index() {
+    let data = useLoaderData<BucketsList>();
+    const buckets = data.buckets || [];
+    return (
+        <main>
+            <h2>Existing buckets</h2>
+            <ol>
+                {buckets.map(bucket => <li key={bucket.name}>{bucket.name}, {bucket.createdAt}</li>)}
+            </ol>
+            <h2>Create a new bucket</h2>
+            <Form method="post">
+                <div>
+                    <label>
+                        Name:{" "}
+                        <input type="text" name="name" required />
+                    </label>
+                </div>
+                <button type="submit">Create</button>
+            </Form>
+        </main>
+    )
+}
